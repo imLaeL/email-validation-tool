02267b7478f5b0a43fc7b448e9e185febe37c716
imLaeL <isaque.lael@academico.ifpb.edu.br>
2023-02-27 15:19:15 -0300

    ver 0.2

--- evpy.py
+++ evpy.py
@@ -4,9 +4,22 @@
 
 import requests
 
+#---------------- PT-BR -----------------------------------
+
 # Acesse https://isitarealemail.com e cadastre-se no site.
 # Depois do cadastro ter sido feito, acesse: https://isitarealemail.com/getting-started/api
 # e use a chave api no script. :)
+
+# A chave API é para fim de testes, talvez eu a deixe no script.
+# ---------------------------------------------------------
+
+# ------------------ EN -----------------------------------
+# 
+# Visit https://isitarealemail.com and register on the site.
+# After registration has been done, go to: https://isitarealemail.com/getting-started/api
+# and use the api key in the script. :)
+
+# The API key is for testing purposes, maybe I'll leave it in the script.
 
 api_key = '4a6bddfb-fbdf-41b4-9e96-34de7d23d5b5'
 
@@ -40,7 +53,7 @@ cod = int(input('Digite o código: '))
 
 if cod == 1:
 	email_indiv()
-#if cod == 2:
+#if cod == 2:		--> Coming Soon 
 	#email_multip()
 elif cod == 3:
 	print('até mais ;)')
